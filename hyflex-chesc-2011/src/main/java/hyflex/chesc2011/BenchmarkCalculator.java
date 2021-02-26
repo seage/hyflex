289d9f8598e15df5a541474a824bde5b194f1b9d
David Omrai <daom9999@gmail.com>
2021-02-26 10:12:23 +0000

    #2 Changed the location of hh results for each run and fixed the benchmarkcalculator, it is now able to run multiple benchmarks on subdirectories of output/results

--- hyflex-chesc-2011/src/main/java/hyflex/chesc2011/BenchmarkCalculator.java
+++ hyflex-chesc-2011/src/main/java/hyflex/chesc2011/BenchmarkCalculator.java
@@ -3,6 +3,7 @@ package hyflex.chesc2011;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -12,12 +13,16 @@ import java.util.Collections;
  * ASAP Research Group, School of Computer Science, University of Nottingham
  * mvh@cs.nott.ac.uk
  *  
- * This program is for comparing future Hyper-Heuristics to the results of the Cross-Domain Heuristic Search Challenge 2011.
- * It takes one hyper-heuristic as input, and enters it into a competition with the 20 hyper-heuristics which were submitted to the competition in 2011.
+ * This program is for comparing future Hyper-Heuristics 
+ * to the results of the Cross-Domain Heuristic Search Challenge 2011.
+ * It takes one hyper-heuristic as input, and enters it into a competition 
+ * with the 20 hyper-heuristics which were submitted to the competition in 2011.
  * 
  * This class should come with a 'submitted' directory, containing 20 files.
- * To use, you will need to make sure that this class is in the same directory as the 'submitted' directory (i.e. outside the submitted directory, not in it).
- * In the competition, each competitor was run 31 times on each instance, so for a fair comparison yours should also be run 31 times.
+ * To use, you will need to make sure that this class is in the same directory 
+ * as the 'submitted' directory (i.e. outside the submitted directory, not in it).
+ * In the competition, each competitor was run 31 times on each instance, 
+ * so for a fair comparison yours should also be run 31 times.
  * The median of the 31 runs is your result on that instance.
  * Obtain the results for your hyper-heuristic on all 6 problem domains (30 instances in total), 
  * and save them in a file in the following format:
@@ -43,161 +48,213 @@ import java.util.Collections;
  * These were the instances used for the 2011 competition
  * 
  * Put your file into the "submitted/" directory, and then run this class. 
- * This class will read the files in the submitted directory, and treat each as a competitor to calculate the scores.
- * If you wish, you may remove files from the submitted directory, which will reduce the competitors used for comparison.
+ * This class will read the files in the submitted directory, 
+ * and treat each as a competitor to calculate the scores.
+ * If you wish, you may remove files from the submitted directory, 
+ * which will reduce the competitors used for comparison.
  *
  * please report any bugs to Dr Matthew Hyde at mvh@cs.nott.ac.uk
  */
 
 public class BenchmarkCalculator {
 
-	public static void main (String[] argv) {
+  /**
+   * Main method of this class, runs benchmarking.
+   * Returns leaderboard based on given hyper-heuristics results.
+   * @param argv given arguments.
+   */
+  public static void main(String[] argv) {
 
-		int domains = 6;
-		int number_of_instances = 5;
+    int domains = 6;
+    int numberOfInstances = 5;
 
-		String pathToSubmitted = "./docs/HyFlex/submitted_results/";
+    File sfile = new File("./output/results/");
+    String[] directories = sfile.list(new FilenameFilter() {
+      @Override
+      public boolean accept(File current, String name) {
+        return new File(current, name).isDirectory();
+      }
+    });
 
-		File dir = new File(pathToSubmitted); 
-		String[] children = dir.list(); 
-		String[] hhnames = null;
-		int hyperheuristics = 0;
-		double[][][] submittedscores = null;
-		if (children == null) { 
-			System.out.println("there are no files in the submitted directory");
-		} else {
-			hyperheuristics = children.length;
-			hhnames = new String[children.length];
-			submittedscores = new double[domains][number_of_instances][hyperheuristics];
-			System.out.println("\nInput files:");
-			for (int file=0; file<children.length; file++) {
-				String filename = children[file];
-				System.out.println(filename);
-				hhnames[file] = filename.split(".txt")[0];
-				try{
-					FileReader read = new FileReader(pathToSubmitted+"/"+filename);
-					BufferedReader buff = new BufferedReader(read);
-					for (int l = 0; l < domains; l++) {
-						String s = buff.readLine();
-						String[] sa = s.split(",");
-						for (int ins = 0; ins < number_of_instances; ins++) {
-							String u = sa[ins];
-							double individualresult = Double.parseDouble(u);
-							submittedscores[l][ins][file] = individualresult;
-							System.out.print(individualresult + " ");
-						}System.out.println();
-					}
-					buff.close();
-					read.close();
-				}catch(IOException e) {
-					System.err.println(e.getMessage());
-					System.exit(-1);
-				}
-			}
-		}
+    for (String directory: directories) {
 
-		double[][][] results = new double[domains][number_of_instances][hyperheuristics];//used to store all of the scores
+      String pathToSubmitted = "./output/results/" + directory;
 
-		//add the scores of the submitted hyper-heuristics
-		for (int d = 0; d < domains; d++) {
-			for (int i = 0; i < number_of_instances; i++) {
-				for (int h = 0; h < hyperheuristics; h++) {
-					results[d][i][h] = submittedscores[d][i][h];
-				}}}
+      File dir = new File(pathToSubmitted); 
+      String[] children = dir.list();     
+      String[] hhnames = null;
+      int hyperheuristics = 0;
+      double[][][] submittedscores = null;
+      if (children == null) { 
+        System.out.println("there are no files in the submitted directory");
+      } else {
+        hyperheuristics = children.length;
+        hhnames = new String[children.length];
+        submittedscores = new double[domains][numberOfInstances][hyperheuristics];
+        System.out.println("\nInput files:");
+        for (int file = 0; file < children.length; file++) {
+          String filename = children[file];
+          System.out.println(filename);
+          hhnames[file] = filename.split(".txt")[0];
+          try {
+            FileReader read = new FileReader(pathToSubmitted + "/" + filename);
+            BufferedReader buff = new BufferedReader(read);
+            for (int l = 0; l < domains; l++) {
+              String s = buff.readLine();
+              String[] sa = s.split(",");
+              for (int ins = 0; ins < numberOfInstances; ins++) {
+                String u = sa[ins];
+                double individualresult = Double.parseDouble(u);
+                submittedscores[l][ins][file] = individualresult;
+                System.out.print(individualresult + " ");
+              }
+              System.out.println();
+            }
+            buff.close();
+            read.close();
+          } catch (IOException e) {
+            System.err.println(e.getMessage());
+            System.exit(-1);
+          }
+        }
+      }
 
+      //used to store all of the scores
+      double[][][] results = new double[domains][numberOfInstances][hyperheuristics];
 
-		double[] scores = new double[hyperheuristics];
-		double[] basescores = {10,8,6,5,4,3,2,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-		double totaldomainscores = 0;
-		for (double g : basescores) {
-			totaldomainscores += g;
-		}totaldomainscores *= number_of_instances;//total available per domain
-		for (int domain = 0; domain < domains; domain++) {
-			double[] domainscores = new double[hyperheuristics];
-			for (int i = 0; i < number_of_instances; i++) {
-				double[] res = results[domain][i];
-				ArrayList<Score> al = new ArrayList<Score>();
-				for (int s = 0; s < res.length; s++) {
-					Score obj = new Score(s, res[s]);
-					al.add(obj);
-				}
-				Collections.sort(al); 
-				double lastscore = Double.POSITIVE_INFINITY;
-				int scoreindex = 0;
-				double tieaverage = 0;
-				int tienumber = 0;
-				ArrayList<Integer> list = new ArrayList<Integer>();
-				while (true) {
-					Score test1 = null;
-					if (scoreindex < al.size()) {
-						test1 = al.get(scoreindex);
-					}
-					if (test1 == null || test1.score != lastscore) {
-						double average = tieaverage / list.size();
-						for (int f = 0; f < list.size(); f++) {
-							domainscores[list.get(f)] += average;
-						}
-						if (test1 == null) {
-							break;//all HH have been given a score
-						}
-						tienumber = 0;
-						tieaverage = 0;
-						list = new ArrayList<Integer>();
-						list.add(test1.num);
-						tienumber++;
-						tieaverage += basescores[scoreindex];
-					} else {
-						list.add(test1.num);
-						tienumber++;
-						tieaverage += basescores[scoreindex];
-					}
-					lastscore = test1.score;
-					scoreindex++;
-				}
-			}
-			String d = "";
-			switch (domain) {
-			case 0: d = "SAT";break;
-			case 1: d = "Bin Packing";break;
-			case 2: d = "Personnel Scheduling";break;
-			case 3: d = "Flow Shop";break;
-			case 4: d = "TSP";break;
-			case 5: d = "VRP";break;}
-			System.out.println(d);
-			double domaintotal = 0;
-			for (int g = 0; g < domainscores.length; g++) {
-				domaintotal += domainscores[g];
-				scores[g] += domainscores[g];
-				System.out.println(hhnames[g] + ", " + domainscores[g]);
-			}System.out.println();
-			if (Math.round(domaintotal) != totaldomainscores) {
-				System.err.println("Error, total scores for this domain ("+domaintotal+") do not add up to " + totaldomainscores);
-				System.err.println("This represents a bug. Please email this java class file to Dr Matthew Hyde at mvh@cs.nott.ac.uk");
-				System.exit(-1);
-			}
-		}
-		System.out.println("Overall Total ");
-		for (int g = 0; g < scores.length; g++) {
-			System.out.println(hhnames[g] + ", " + scores[g]);
-		}
-	}
+      //add the scores of the submitted hyper-heuristics
+      for (int d = 0; d < domains; d++) {
+        for (int i = 0; i < numberOfInstances; i++) {
+          for (int h = 0; h < hyperheuristics; h++) {
+            results[d][i][h] = submittedscores[d][i][h];
+          }
+        }
+      }
 
-	public static class Score implements Comparable<Score> {
-		int num;
-		double score;
-		public Score(int n, double s) {
-			num = n;
-			score = s;
-		}
-		public int compareTo(Score o) {
-			Score obj = (Score)o;
-			if (this.score < obj.score) {
-				return -1;
-			} else if (this.score == obj.score) {
-				return 0;
-			} else {
-				return 1;
-			}
-		}
-	}
+      double[] scores = new double[hyperheuristics];
+      double[] basescores = {10,8,6,5,4,3,2,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+      // double totalDomainScores = 0;
+      // 
+      // for (double g : basescores) {
+      //   totalDomainScores += g;
+      // }
+      // totalDomainScores *= numberOfInstances;//total available per domain
+      // 
+      for (int domain = 0; domain < domains; domain++) {
+        double[] domainscores = new double[hyperheuristics];
+        for (int i = 0; i < numberOfInstances; i++) {
+          double[] res = results[domain][i];
+          ArrayList<Score> al = new ArrayList<Score>();
+          for (int s = 0; s < res.length; s++) {
+            Score obj = new Score(s, res[s]);
+            al.add(obj);
+          }
+          Collections.sort(al); 
+          double lastscore = Double.POSITIVE_INFINITY;
+          int scoreindex = 0;
+          double tieaverage = 0;
+          //int tieNumber = 0;
+          ArrayList<Integer> list = new ArrayList<Integer>();
+          while (true) {
+            Score test1 = null;
+            if (scoreindex < al.size()) {
+              test1 = al.get(scoreindex);
+            }
+            if (test1 == null || test1.score != lastscore) {
+              double average = tieaverage / list.size();
+              for (int f = 0; f < list.size(); f++) {
+                domainscores[list.get(f)] += average;
+              }
+              if (test1 == null) {
+                break;//all HH have been given a score
+              }
+              //tieNumber = 0;
+              tieaverage = 0;
+              list = new ArrayList<Integer>();
+              list.add(test1.num);
+              //tieNumber++;
+              tieaverage += basescores[scoreindex];
+            } else {
+              list.add(test1.num);
+              //tieNumber++;
+              tieaverage += basescores[scoreindex];
+            }
+            lastscore = test1.score;
+            scoreindex++;
+          }
+        }
+        String d = "";
+        switch (domain) {
+          case 0: 
+            d = "SAT";
+            break;
+          case 1: 
+            d = "Bin Packing";
+            break;
+          case 2: 
+            d = "Personnel Scheduling";
+            break;
+          case 3: 
+            d = "Flow Shop";
+            break;
+          case 4: 
+            d = "TSP";
+            break;
+          case 5: 
+            d = "VRP";
+            break;
+          default:  
+            break;
+        }
+        System.out.println(d);
+        //double domainTotal = 0;
+        for (int g = 0; g < domainscores.length; g++) {
+          //domainTotal += domainscores[g];
+          scores[g] += domainscores[g];
+          System.out.println(hhnames[g] + ", " + domainscores[g]);
+        }
+        System.out.println();
+        /*
+        if (Math.round(domainTotal) != totalDomainScores) {
+          System.err.println("Error, total scores for this domain 
+          (" + domainTotal + ") do not add up to " + totalDomainScores);
+          System.err.println("This represents a bug. Please email this
+          java class file to Dr Matthew Hyde at mvh@cs.nott.ac.uk");
+          System.exit(-1);
+        }
+        */
+      }
+      System.out.println("------------------------------------------");
+      System.out.println("Overall Total " + directory + " ");
+      for (int g = 0; g < scores.length; g++) {
+        System.out.println(hhnames[g] + ", " + scores[g]);
+      }
+      System.out.println("------------------------------------------");
+    }
+  }
+
+  public static class Score implements Comparable<Score> {
+    int num;
+    double score;
+
+    public Score(int n, double s) {
+      num = n;
+      score = s;
+    }
+    
+    /**
+     * Method compares score of current object to given one.
+     * @param o given score to compare
+     */
+    public int compareTo(Score o) {
+      Score obj = (Score)o;
+      if (this.score < obj.score) {
+        return -1;
+      } else if (this.score == obj.score) {
+        return 0;
+      } else {
+        return 1;
+      }
+    }
+  }
 }
---
+++ hyflex-chesc-2011/src/main/java/hyflex/chesc2011/HyperHeuristicRunner.java
@@ -0,0 +1,146 @@
+package hyflex.chesc2011;
+
+/**
+ * 
+ * An example of how to use Lean-GIHH to solve an instance of the HyFlex benchmark.
+ * 
+ * @author Steven Adriaensen
+ * 
+ */
+import AbstractClasses.HyperHeuristic;
+import AbstractClasses.ProblemDomain;
+import SAT.SAT;
+
+import be.kuleuven.kahosl.acceptance.AcceptanceCriterionType;
+import be.kuleuven.kahosl.hyperheuristic.GIHH;
+import be.kuleuven.kahosl.selection.SelectionMethodType;
+
+import fr.lalea.eph.EPH;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+
+import kubalik.EvoCOPHyperHeuristic;
+import leangihh.LeanGIHH;
+import pearlhunter.PearlHunter;
+
+import travelingSalesmanProblem.TSP;
+
+public class HyperHeuristicRunner {
+  /**
+   * Main method of HyperHeuristicRunner class, it runs hyper-heuristic based on the user input.
+   * @param args This example takes no command line arguments
+   */
+  public static void main(String[] args) {
+    if (args.length < 3) {
+      System.out.println("ERROR, not enough arguments, " + "(" + args.length + "/3)");
+      return;
+    }
+    if (args.length > 3) {
+      System.out.println("ERROR, too many arguments, " + "(" + args.length + "/3)");
+      return;
+    }
+    if (args[2].matches("\\d+") == false) {
+      System.out.println("ERROR, wrong number format");
+      return;
+    }
+    //seed for random generatortor
+    long seed = new Date().getTime();
+    /**
+     * Hyper heuristic to use.
+     * ----------------------
+     * GIHH (the winner)
+     * LeanGIHH
+     * PearlHunter
+     * EPH
+     * ISEA
+     */
+    final String algorithmName = args[0];
+    
+    /**
+     * Problems.
+     * -----------------
+     * SAT
+     * TSP
+     */
+    final String ProblemName = args[1];
+    
+    int[] sat = {3,5,4,10,11};
+    int[] tsp = {0,8,2,7,6};
+    HashMap<String, int[]> instances = new HashMap<>();
+    instances.put("SAT", sat);
+    instances.put("TSP", tsp);
+
+    //time we're allowed to optimize (600000ms = 10min)
+    final long timeAllowed = Integer.parseInt(args[2]) * 1000;
+    
+    List<Double> output = new ArrayList<Double>();
+
+    for (int i = 0; i < instances.get(ProblemName).length; i++) {
+      ProblemDomain problem = HyperHeuristicRunner.createProblem(ProblemName, seed);
+      HyperHeuristic algorithm = HyperHeuristicRunner.createAlgorithm(
+          problem, algorithmName, seed, timeAllowed
+      );
+      
+      int instanceIx = instances.get(ProblemName)[i];
+
+      algorithm.setTimeLimit(timeAllowed);
+      algorithm.loadProblemDomain(problem);
+      problem.loadInstance(instanceIx);      
+
+      //start optimizing
+      System.out.println("Testing " + algorithm + " for " + timeAllowed 
+          + " ms on " + problem.getClass().getSimpleName() + "[" + instanceIx + "]...");
+      algorithm.run();
+
+      //print out quality of best solution found
+      System.out.println(algorithm.getBestSolutionValue());
+      output.add(algorithm.getBestSolutionValue());
+    }
+    System.out.println(output);
+  }
+
+  private static HyperHeuristic createAlgorithm(
+      ProblemDomain problem, 
+      String algorithmName, 
+      long seed, 
+      long timeout
+  ) {
+    switch (algorithmName) {
+      case "GIHH":
+        return new GIHH(
+        seed, 
+        problem.getNumberOfHeuristics(), 
+        timeout,
+        "gihh",
+        SelectionMethodType.AdaptiveLimitedLAassistedDHSMentorSTD, 
+        AcceptanceCriterionType.AdaptiveIterationLimitedListBasedTA);
+      case "LeanGIHH":
+        return new LeanGIHH(seed);
+      case "PearlHunter":
+        return new PearlHunter(seed);
+      case "EPH":
+        return new EPH(seed);
+      case "ISEA":
+        return new EvoCOPHyperHeuristic(seed);
+      default:
+        System.out.println("ERROR, " + algorithmName + " INVALID INPUT");
+        return null;
+    }
+  }
+
+  private static ProblemDomain createProblem(String problemName, long seed) {
+    switch (problemName) {
+      case "SAT":
+        return new SAT(seed);
+      case "TSP":
+        return new TSP(seed);
+      default:
+        System.out.println("ERROR, " + problemName + " INVALID INPUT");
+        return null;
+    }
+  }
+
+}
