69e7f86d9a7f21a82a3538fe11f4efcdce3b6942
Richard Malek <rick@rickq.net>
2021-03-26 13:17:55 +0100

    Added a junit test

---
+++ hyflex-chesc-2011/src/test/java/hyflex/chesc2011/MetricTest.java
@@ -0,0 +1,22 @@
+package hyflex.chesc2011;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+// See https://dzone.com/articles/junit-tutorial-for-beginners-in-5-steps
+
+public class MetricTest {
+
+  @Test
+  void testMetric() {
+    assertEquals(1, 1);
+    assertTrue(false);
+  }
+}
