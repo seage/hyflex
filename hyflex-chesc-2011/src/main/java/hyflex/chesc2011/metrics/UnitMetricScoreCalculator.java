2f89aa2ba9ec6534e611aee933df4d4ce773f4c0
Richard Malek <rick@rickq.net>
2021-03-29 13:04:46 +0200

    #13 Added ScoreCalculator interface and F1MetricScoreCalculator empty implementation

--- hyflex-chesc-2011/src/main/java/hyflex/chesc2011/metrics/UnitMetricScoreCalculator.java
+++ hyflex-chesc-2011/src/main/java/hyflex/chesc2011/metrics/UnitMetricScoreCalculator.java
@@ -11,7 +11,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class UnitMetricScoreCalculator {
+public class UnitMetricScoreCalculator implements ScoreCalculator{
   // Array of problems domains ids.
   String[] problems;
   // Path to metadata xml files.
