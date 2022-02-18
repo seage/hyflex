183483928cc66d347ecd271db286d3b94ee568fc
David Omrai <david.omrai@seznam.cz>
2022-02-18 16:29:05 +0100

    #29 Prettified the code a bit, devide the long code to smaller functions

--- docs/pages/main.py
+++ docs/pages/main.py
@@ -1,40 +1,58 @@
+#! /usr/bin/python
+# @author David Omrai
+
+from cmath import exp
 from importlib.abc import FileLoader
 from xml.dom import minidom
 from jinja2 import Environment, FileSystemLoader 
+import sys
 
-# Parse xml file
-results_xml = minidom.parse("results/1/unit-metric-scores.xml").getElementsByTagName('results')[0]
+def exp_xml_to_dict(exp_xml_path):
+    # Parse xml file
+    results_xml = minidom.parse(exp_xml_path).getElementsByTagName('results')[0]
 
-algorithms_xml =  results_xml.getElementsByTagName("algorithm") 
-problems_xml =algorithms_xml[0].getElementsByTagName("problem")
+    algorithms_xml =  results_xml.getElementsByTagName("algorithm") 
+    problems_xml =algorithms_xml[0].getElementsByTagName("problem")
 
-# Extract the data from xml format
-problems = []
+    # Extract the data from xml format
+    problems = []
 
-for problem_xml in problems_xml:
-    problems.append(problem_xml.attribute["name"])
+    for problem_xml in problems_xml:
+        problems.append(problem_xml.attribute["name"])
 
-results = []
+    results = []
 
-for algorithm_xml in algorithms_xml:
-    result = {}
-    
-    # Algorithm name
-    result["name"] = algorithm_xml.attribute["name"]
+    for algorithm_xml in algorithms_xml:
+        result = {}
+        
+        # Algorithm name
+        result["name"] = algorithm_xml.attribute["name"]
 
-    # Algorithm overall score
-    result["overall"] = algorithm_xml.attribute["score"]
+        # Algorithm overall score
+        result["overall"] = algorithm_xml.attribute["score"]
 
-    # Score on each problem
-    result["score"] = {}
-    for score_xml in algorithm_xml.getElementsByTagName("problem"):
-        result["score"][score_xml.attribute["name"]] = score_xml.attribute["avg"]
-    
+        # Score on each problem
+        result["score"] = {}
+        for score_xml in algorithm_xml.getElementsByTagName("problem"):
+            result["score"][score_xml.attribute["name"]] = score_xml.attribute["avg"]
+        
+        results.append(result)
 
-    results.append[result]
+
+def create_page(results, page_dest):
+    # Jinja2 part for templates
+    file_loader = FileSystemLoader("templates")
+    env = Environment(loader=file_loader)
+
+    rendered = env.get_template("docs/pages/templates/results.html").render()
+
+    with open("{page_dest}", "w") as f:
+        f.write(rendered)
+
+def build_results_page(exp_id):
+    results = exp_xml_to_dict("results/{exp_id}/unit-metric-scores.xml")
+    create_page(results, "results/{exp_id}/index.html")
 
 
-# fileLoader = FileSystemLoader("templates")
-# env = Environment(loader=fileLoader)
-
-# rendered = env.get_template("results.html").render()
+if __name__ == "__main__":
+    build_results_page(int(sys.argv[1]))
