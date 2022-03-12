ef9e6c24c2ee17874e95857b607ea74b2599d3a9
Richard Malek <rick@rickq.net>
2022-03-12 23:44:04 +0100

    #35 Tiny formatting

--- hyflex-chesc-2011/src/main/java/hyflex/chesc2011/CompetitionRunner.java
+++ hyflex-chesc-2011/src/main/java/hyflex/chesc2011/CompetitionRunner.java
@@ -430,7 +430,7 @@ public class CompetitionRunner extends Thread {
     final int[] sat = {3,5,4,10,11};
     final int[] bp  = {7,1,9,10,11};
     final int[] ps  = {5,9,8,10,11};
-    final int[] fsp  = {1,8,3,10,11};
+    final int[] fsp = {1,8,3,10,11};
     final int[] tsp = {0,8,2,7,6};
     final int[] vrp = {6,2,5,1,9};
     final int[] qap = {0,1,2,3,9};
