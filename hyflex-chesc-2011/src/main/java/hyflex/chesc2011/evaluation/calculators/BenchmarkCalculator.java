a083e768545058aac41dadf9d2079996499fba29
Richard Malek <rick@rickq.net>
2022-03-21 12:56:23 +0100

    #38 Minor fix

--- hyflex-chesc-2011/src/main/java/hyflex/chesc2011/evaluation/calculators/BenchmarkCalculator.java
+++ hyflex-chesc-2011/src/main/java/hyflex/chesc2011/evaluation/calculators/BenchmarkCalculator.java
@@ -10,8 +10,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.logging.Logger;
